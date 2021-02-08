34200878f0a47640d71a89fa144906e067e1c455
NORAH-HURUBI <nan8794@gmail.com>
2021-02-08 21:03:38 +0300

    updateTest

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
 def _logger():
     '''
-    Setup logger format, level, and handler.
+    Setup logger format, level, and handlert.
 
     RETURNS: log object
     '''
